891acd8e89c9bb19b7b55c5b21ac238becb3dc49
Lanqilu <885240677@qq.com>
2021-09-29 11:25:16 +0800

    feat: 使用配置文件自定义用户名和密码

---
+++ SpringSecurity/Security-01-HelloSecurity/src/main/java/com/halo/hello/HelloSecurity01Application.java
@@ -0,0 +1,16 @@
+package com.halo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Halo
+ * @date Created in 2021/04/15 3:51 PM
+ */
+@SpringBootApplication
+public class HelloSecurity01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSecurity01Application.class, args);
+    }
+}
+
