0507e69d24c592af1eafc6a313908dad8d88f0df
Lanqilu <885240677@qq.com>
2021-10-08 14:52:18 +0800

    feat: hasAuthority 方法

---
+++ SpringSecurity/Authority-01-HelloAuthority/src/main/java/com/halo/hello/HelloAuthority01Application.java
@@ -0,0 +1,16 @@
+package com.halo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Halo
+ * @date Created in 2021/04/15 3:51 PM
+ * @description 自定义实现类配置用户名和密码
+ */
+@SpringBootApplication
+public class HelloAuthority01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAuthority01Application.class, args);
+    }
+}
