c2877aa7483d5492cdb6ddc5ed1e28e3a34b4c05
Lanqilu <885240677@qq.com>
2021-10-08 15:13:06 +0800

    feat: 退出登录

---
+++ SpringSecurity/Authority-05-Logout/src/main/java/com/halo/hello/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.halo.hello.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.halo.hello.entity.Users;
+import com.halo.hello.mapper.UsersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Halo
+ * @create 2021/10/08 上午 11:09
+ * @description
+ */
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 调用 usersMapper 方法，根据用户名查询数据库
+        QueryWrapper<Users> wrapper = new QueryWrapper();
+        // where username=?
+        wrapper.eq("username", username);
+        Users users = usersMapper.selectOne(wrapper);
+        // 判断,数据库是否存在用户名，认证失败
+        if (users == null) {
+            System.out.println("认证失败");
+            throw new UsernameNotFoundException("用户名不存在！");
+        }
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("admins,ROLE_user");
+        // 从查询数据库返回 users 对象，得到用户名和密码，返回
+        return new User(users.getUsername(), new BCryptPasswordEncoder().encode(users.getPassword()), auths);
+    }
+}
