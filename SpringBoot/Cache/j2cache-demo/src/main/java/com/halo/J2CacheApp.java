b0beecef72161dd2ad4ca03760719aceabe638b6
lanqilu <885240677@qq.com>
2021-12-02 15:20:45 +0800

    添加 J2Cache 缓存的使用案例

---
+++ SpringBoot/Cache/j2cache-demo/src/main/java/com/halo/J2CacheApp.java
@@ -0,0 +1,16 @@
+package com.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Halo
+ * @create 2021/11/30 下午 04:08
+ * @description
+ */
+@SpringBootApplication
+public class J2CacheApp {
+    public static void main(String[] args) {
+        SpringApplication.run(J2CacheApp.class,args);
+    }
+}
